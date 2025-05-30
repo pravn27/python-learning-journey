6dac12fdd3f0cc3de391b9aeebb3a6bb07a25cc2
pravn27 <praveen.sri27@gmail.com>
2025-05-30 19:48:19 +0530

    python basics

---
+++ krish-naik/1 - Python Basics/basic.py
@@ -0,0 +1,12 @@
+## this is single line comment
+userName = 'praveen'
+print(userName)
+
+'''
+this is multi line comment
+This is a multi-line comment.
+This is the second line of the multi-line comment.
+This is the third line of the multi-line comment.
+'''
+
+print("Hello World")
