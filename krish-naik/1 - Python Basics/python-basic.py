040f6379b1a3f868d668ed8ab46449d44efefca9
pravn27 <praveen.sri27@gmail.com>
2025-06-14 10:57:39 +0530

    python fundamentals

--- krish-naik/1 - Python Basics/python-basic.py
+++ krish-naik/1 - Python Basics/python-basic.py
@@ -9,7 +9,14 @@ This is the second line of the multi-line comment.
 This is the third line of the multi-line comment.
 '''
 
+
 print("Hello World")
+
+# line continuation
+total_price = 10 + 20 + 30 + \
+    40 + 50
+print(f"total price is {total_price}")
+
 
 # Python is a case sensitive language
 name = 'Praveen'
@@ -17,5 +24,13 @@ Name = 'Shekarappa'
 print(name)
 print(Name)
 
+first_name = "Praveen"
+is_user_logged_in = True
+print(first_name.upper())
 
-
+# python is a dynamically typed language / type inference
+# variable type can change at runtime
+user_name = "Praveen"
+print(type(user_name))
+user_name = 10
+print(type(user_name))
